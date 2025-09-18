59def7fb9e5a763b4ef6970923b2aebef13515fe
NK <knagisheva@gmail.com>
2025-09-18 11:17:31 +0300

    F

---
+++ Assets/Scripts/Phisycs.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phisycs : MonoBehaviour
+{ 
+    [SerializeField] private List<Force> _forces = new();
+
+    public void OnDrawGizmos()
+    {
+        foreach (Force force in _forces)
+        {
+            Gizmos.color = force.ColorForce;
+            Vector3 start = transform.position;
+            Vector3 end = transform.position + force.Vector;
+
+            Gizmos.DrawLine(start, end);
+        }
+    }
+}
+
+
+[Serializable]
+public class Force
+{
+
+    public Vector3 Vector;
+    public Color ColorForce;
+    public string Name;
+
+    public Force (Vector3 vector, Color colorForce, string name)
+    {
+        Vector = vector;
+        ColorForce = colorForce;
+        Name = name;
+    }
+}
