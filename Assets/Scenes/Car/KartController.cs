f1ee1f03fa9c3d001dcb1b8294254d255ac0d0f4
Ксения Нагишева <knagisheva@gmail.com>
2025-12-17 00:58:55 +0300

    Kart upgrade

--- Assets/Scenes/Car/KartController.cs
+++ Assets/Scenes/Car/KartController.cs
@@ -1,196 +1,252 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody))]
-
 public class KartController : MonoBehaviour
 {
-   [SerializeField] private Transform _frontLeftWheel;
-   [SerializeField] private Transform _frontRightWheel;
-   [SerializeField] private Transform _rearLeftWheel;
-   [SerializeField] private Transform _rearRightWheel;
+    [Header("Import parametrs")]
+    [SerializeField] private bool _import = false;
+    [SerializeField] private KartConfig _kartConfig;
 
-   [SerializeField] private InputActionAsset _playerInput;
+    [Header("Wheel attachment points")]
+    [SerializeField] private Transform _frontLeftWheel;
+    [SerializeField] private Transform _frontRightWheel;
+    [SerializeField] private Transform _rearLeftWheel;
+    [SerializeField] private Transform _rearRightWheel;
 
-   [SerializeField, Range(0, 1)] private float _frontAxisShare = 0.5f;
-   private InputAction _moveAction;
+    [Header("Input (New Input System)")]
+    [SerializeField] private InputActionAsset _playerInput;
 
-   private float _throttleInput;
-   private float _steerInput;
+    [Header("Weight distribution")]
+    [SerializeField, Range(0, 1)] private float _frontAxisShare = 0.5f;
 
-   private float _frontLeftNormalForce, _frontRightNormalForce, _rearLeftNormalForce, _rearRightNormalForce;
-
-   private Rigidbody _rigidbody;
-
-   private Vector3 g = Physics.gravity;
-
-   [SerializeField] private float engineTorque = 400f; // N*m
-   [SerializeField] private float wheelRadius = 0.3f;
-   [SerializeField] private KartEngine engine;
-   [SerializeField] private float gearRatio =8f;
-   [SerializeField] private float drivetrainEfficency =0.9f;
-   [SerializeField] private float maxSpeed = 20;
-
-   [SerializeField] private float maxSteeringAngle;
-   private Quaternion frontLeftInitialRot;
-   private Quaternion frontRightInitialRot;
-
-   [SerializeField] private float frictionCoefficient = 1f;
-   [SerializeField] private float lateralStiffnes = 80f;
-   [SerializeField] private float rollingResistance =30f;
-
-   private bool isGround = false;
-
-   private void OnTriggerEnter(Collider other)
-   {
-      if (other.tag == "Floor") isGround = true;
-   }
-   
-   private void OnTriggerExit(Collider other)
-   {
-      if (other.tag == "Floor") isGround = false;
-   }
-
-   private void Awake()
-   {
-      _playerInput.Enable();
-      _rigidbody = GetComponent<Rigidbody>();
-      var map = _playerInput.FindActionMap("Kart");
-      _moveAction = map.FindAction("Move");
-
-      frontLeftInitialRot = _frontLeftWheel.localRotation;
-      frontRightInitialRot = _frontRightWheel.localRotation;
-
-      ComputeStaticWheelLoad();
-   }
+    [Header("Engine & drivetrain")]
+    [SerializeField] private KartEngine _engine;
+    [SerializeField] private float _gearRatio = 8f;
+    [SerializeField] private float _drivetrainEfficiency = 0.9f;
 
 
-   private void OnDisable()
-   {
-      _playerInput.Disable();
-   }
+    [Header("Handbrake")]
+    [SerializeField] private KeyCode handbrakeKey = KeyCode.Space;
+    [SerializeField] private float handbrakeBrakeForce = 6000f; 
 
-   void RotateFrontWheels()
-   {
-      float steerAngle = maxSteeringAngle * _steerInput;
-      Quaternion steerRot = Quaternion.Euler(0, steerAngle, 0);
-      _frontLeftWheel.localRotation = frontLeftInitialRot * steerRot;
-      _frontRightWheel.localRotation = frontRightInitialRot * steerRot;
-   }
+    private InputAction _moveAction;
+    private float _throttleInput;
+    private float _steepInput;
+    private bool _handbrakePressed;
 
-   private void Update()
-   {
-      if (isGround)
-      {
-         ReadInput();
-         RotateFrontWheels();
-      }
-   }
+    private float _frontLeftNormalForce, _frontRightNormalForce, _rearLeftNormalForce, _rearRightNormalForce;
+    private Rigidbody _rigidbody;
+    private Vector3 g = Physics.gravity;
 
-   private void ReadInput()
-   {
-      Vector2 move = _moveAction.ReadValue<Vector2>();
-      _steerInput = Mathf.Clamp(move.x, -1, 1);
-      _throttleInput = Mathf.Clamp(move.y, -1, 1);
+    [SerializeField] private float engineTorque = 400f;
+    [SerializeField] private float wheelRadius = 0.3f;
+    [SerializeField] private float maxSpeed = 20;
 
-   }
+    [Header("Steering")]
+    [SerializeField] private float maxSteeringAngle;
 
-   void ComputeStaticWheelLoad()
-   {
-      float mass = _rigidbody.mass;
-      float totalWeight = mass * Mathf.Abs(g.y);
+    private Quaternion frontLeftInitialRot;
+    private Quaternion frontRightInitialRot;
 
-      float frontWeight = totalWeight * _frontAxisShare;
-      float rearWeight = totalWeight - frontWeight;
+    [Header("Tyre friction")]
+    [SerializeField] private float frictionCoefficient = 1f;
+    [SerializeField] private float lateralStiffnes = 80f;
+    [SerializeField] private float rollingResistance;
 
-      _frontRightNormalForce = frontWeight * 0.5f;
-      _frontLeftNormalForce = _frontRightNormalForce;
-      _rearRightNormalForce = rearWeight * 0.5f;
-      _rearLeftNormalForce = _rearRightNormalForce;
-   }
+    private float speedAlongForward = 0f;
+    private float Fx = 0f;
+    private float Fy = 0f;
 
-   private void ApplyEngineForces()
-   {
-      Vector3 forward = transform.forward;
-      float speedAlongForward = Vector3.Dot(_rigidbody.linearVelocity, forward);
+    private void Awake()
+    {
+        _playerInput.Enable();
+        _rigidbody = GetComponent<Rigidbody>();
+        var map = _playerInput.FindActionMap("Kart");
+        _moveAction = map.FindAction("Move");
 
-      if (_throttleInput > 0 && speedAlongForward > maxSpeed) return;
+        if (_import) Initialize();
 
-      float driveTorque = engine.Simulate(_throttleInput, speedAlongForward, gearRatio, wheelRadius, Time.fixedDeltaTime);
+        frontLeftInitialRot = _frontLeftWheel.localRotation;
+        frontRightInitialRot = _frontRightWheel.localRotation;
+        ComputeStaticWheelLoad();
+    }
 
-      float driveForcePerWheel = driveTorque / wheelRadius / 2;
+    private void Initialize()
+    {
+        if (_kartConfig != null)
+        {
+            _rigidbody.mass = _kartConfig.mass;
+            frictionCoefficient = _kartConfig.frictionCoefficient;
+            rollingResistance = _kartConfig.rollingResistance;
+            maxSteeringAngle = _kartConfig.maxSteerAngle;
+            _gearRatio = _kartConfig.gearRatio;
+            wheelRadius = _kartConfig.wheelRadius;
+            lateralStiffnes = _kartConfig.lateralStiffness;
+        }
+    }
 
-      Vector3 forceRearLeft = forward * driveForcePerWheel;
-      Vector3 forceRearRight = forceRearLeft;
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
 
-      _rigidbody.AddForceAtPosition(forceRearLeft, _rearLeftWheel.position, ForceMode.Force);
-      _rigidbody.AddForceAtPosition(forceRearRight, _rearRightWheel.position, ForceMode.Force);
-   }
+    private void Update()
+    {
+        ReadInput();
+        RotateFrontWheels();
+    }
 
-   private void FixedUpdate()
-   {
-      if (isGround)
-      {
-         ApplyEngineForces();
-         ApplyWheelForce(_frontLeftWheel, _frontLeftNormalForce, true, false);
-         ApplyWheelForce(_frontRightWheel, _frontRightNormalForce, true, false);
-         ApplyWheelForce(_rearLeftWheel, _rearLeftNormalForce, false, true);
-         ApplyWheelForce(_rearRightWheel, _rearRightNormalForce, false, true);
-      }
-   }
+    private void ReadInput()
+    {
+        Vector2 move = _moveAction.ReadValue<Vector2>();
+        _steepInput = Mathf.Clamp(move.x, -1, 1);
+        _throttleInput = Mathf.Clamp(move.y, -1, 1);
 
-   void ApplyWheelForce(Transform wheel, float normalForce, bool isSteer, bool isDrive)
-   {
-      Vector3 wheelPos = wheel.position;
-      Vector3 wheelForward = wheel.forward;
-      Vector3 wheelRight = wheel.right;
+        _handbrakePressed = Input.GetKey(handbrakeKey);  // Ручник
+    }
 
-      Vector3 velocity = _rigidbody.GetPointVelocity(wheelPos);
+    void RotateFrontWheels()
+    {
+        float steerAngle = maxSteeringAngle * _steepInput;
+        Quaternion steerRot = Quaternion.Euler(0, steerAngle, 0);
+        _frontLeftWheel.localRotation = frontLeftInitialRot * steerRot;
+        _frontRightWheel.localRotation = frontRightInitialRot * steerRot;
+    }
+    
 
-      float vlong = Vector3.Dot(velocity, wheelForward);
-      float vlat = Vector3.Dot(velocity, wheelRight);
+    void ComputeStaticWheelLoad()
+    {
+        float mass = _rigidbody.mass;
+        float totalWeight = mass * Mathf.Abs(g.y);
+        float frontWeight = totalWeight * _frontAxisShare;
+        float rearWeight = totalWeight - frontWeight;
+        _frontRightNormalForce = frontWeight * 0.5f;
+        _frontLeftNormalForce = _frontRightNormalForce;
+        _rearRightNormalForce = rearWeight * 0.5f;
+        _rearLeftNormalForce = _rearRightNormalForce;
+    }
 
-      float Fx = 0f;
-      float Fy = 0f;
+    private void ApplyEngineForces()
+    {
+        Vector3 forward = transform.forward;
+        float speedAlongForward = Vector3.Dot(_rigidbody.linearVelocity, forward);
+        if (_throttleInput > 0 && speedAlongForward > maxSpeed) return;
 
-      if (isDrive)
-      {
-         Vector3 bodyForward = transform.forward;
-         float speedAlongForward = Vector3.Dot(_rigidbody.linearVelocity, bodyForward);
-         if (!(_throttleInput > 0) && speedAlongForward > maxSpeed)
-         {
-                float engineTorque = engine.Simulate(_throttleInput, speedAlongForward, gearRatio, wheelRadius, Time.fixedDeltaTime) * _throttleInput;
+        float driveTorque = engineTorque * _throttleInput;
+        float driveForcePerWheel = driveTorque / wheelRadius / 2;
+        Vector3 forceRear = forward * driveForcePerWheel;
 
-                float totalWheelTorque = engineTorque * gearRatio * drivetrainEfficency;
-                float wheelTorque = totalWheelTorque / 2;
+        _rigidbody.AddForceAtPosition(forceRear, _rearLeftWheel.position, ForceMode.Force);
+        _rigidbody.AddForceAtPosition(forceRear, _rearRightWheel.position, ForceMode.Force);
+    }
 
-                Fx += wheelTorque;
-         }
-         float rolling = -rollingResistance * vlong;
-         Fy += rolling;
-      }
-      
-      else
-      {
-         float rolling = -rollingResistance * vlong;
-         Fy += rolling;
+    private void FixedUpdate()
+    {
+        ApplyEngineForces();
 
-      }
+        ApplyWheelForce(_frontLeftWheel, _frontLeftNormalForce, isSteer: true, isDrive: false);
+        ApplyWheelForce(_frontRightWheel, _frontRightNormalForce, isSteer: true, isDrive: false);
+        ApplyWheelForce(_rearLeftWheel, _rearLeftNormalForce, isSteer: false, isDrive: true);
+        ApplyWheelForce(_rearRightWheel, _rearRightNormalForce, isSteer: false, isDrive: true);
+    }
 
-      float Fyraw = -lateralStiffnes * vlat;
-      Fy += Fyraw;
-      float frictionLimit = frictionCoefficient * normalForce;
-      float forceLenght = Mathf.Sqrt(Fx*Fx+Fy*Fy);
+    void ApplyWheelForce(Transform wheel, float normalForce, bool isSteer, bool isDrive)
+    {
+        Vector3 wheelPos = wheel.position;
+        Vector3 wheelForward = wheel.forward;
+        Vector3 wheelRight = wheel.right;
+        Vector3 velocity = _rigidbody.GetPointVelocity(wheelPos);
+        float vlong = Vector3.Dot(velocity, wheelForward);
+        float vlat = Vector3.Dot(velocity, wheelRight);
 
-      if (forceLenght > frictionLimit)
-      {
-         float scale = frictionLimit / forceLenght;
-         Fy *= scale;
-         Fx *= scale;
-      }
+        Fx = 0f;
+        Fy = 0f;
 
-      Vector3 force = wheelForward * Fx + wheelForward * Fy;
-      _rigidbody.AddForceAtPosition(force, wheel.position, ForceMode.Force);
-   }
+        if (isDrive)
+        {
+            speedAlongForward = Vector3.Dot(_rigidbody.linearVelocity, transform.forward);
+            float engineTorqueOut = _engine.Simulate(_throttleInput, speedAlongForward, Time.fixedDeltaTime);
+            float totalWheelTorque = engineTorqueOut * _gearRatio * _drivetrainEfficiency;
+            float wheelTorque = totalWheelTorque * 0.5f;
+            Fx += wheelTorque / wheelRadius;
+
+
+            if (_handbrakePressed)
+            {
+                float brakeDir = vlong > 0 ? -1f : (vlong < 0 ? 1f : -1f);
+                Fx += brakeDir * handbrakeBrakeForce;
+            }
+        }
+        else if (isSteer)
+        {
+            float rooling = -rollingResistance * vlong;
+            Fx += rooling;
+        }
+
+        float fyRaw = -lateralStiffnes * vlat;
+        Fy += fyRaw;
+
+        float frictionlimit = frictionCoefficient * normalForce;
+        float forceLenght = Mathf.Sqrt(Fx * Fx + Fy * Fy);
+
+        if (forceLenght > frictionlimit)
+        {
+            float scale = frictionlimit / forceLenght;
+            Fy += scale;
+            Fx += scale;
+        }
+
+
+        Vector3 force = wheelForward * Fx + wheelRight * Fy;
+        _rigidbody.AddForceAtPosition(force, wheel.position, ForceMode.Force);
+    }
+
+
+    void OnGUI()
+    {
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.fontSize = 24;
+        style.normal.textColor = Color.black;
+        style.fontStyle = FontStyle.Bold;
+
+        GUIStyle backgroundStyle = new GUIStyle();
+        backgroundStyle.padding = new RectOffset(15, 15, 10, 10);
+        
+
+        GUILayout.BeginArea(new Rect(20, 20, 400, 330), backgroundStyle);
+        GUILayout.BeginVertical();
+
+        style.fontSize = 20;
+        GUILayout.Space(10);
+        GUILayout.Label($"Speed: {speedAlongForward:0.0} m/s ({(speedAlongForward * 3.6f):0.0} km/h)", style);
+        GUILayout.Label($"Engine RPM: {_engine.CurrentRpm:0} RPM", style);
+
+        GUILayout.Label($"Engine Torque: {_engine.CurrentTorque:0.0} N·m", style);
+
+
+        if (_handbrakePressed)
+        {
+            GUILayout.Label("HANDBRAKE ON!", style);
+        }
+
+        GUILayout.Space(10);
+        GUILayout.Label("Wheel Forces:", style);
+        style.fontSize = 18;
+        GUILayout.Label($"Fx: {Fx:0.0} N");
+        GUILayout.Label($"Fy: {Fy:0.0} N");
+
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+
+    private Texture2D MakeTex(int width, int height, Color col)
+    {
+        Color[] pix = new Color[width * height];
+        for (int i = 0; i < pix.Length; ++i) pix[i] = col;
+        Texture2D result = new Texture2D(width, height);
+        result.SetPixels(pix);
+        result.Apply();
+        return result;
+    }
 }
--- Assets/Scenes/Car/KartEngine.cs
+++ Assets/Scenes/Car/KartEngine.cs
@@ -2,33 +2,93 @@ using UnityEngine;
 
 public class KartEngine : MonoBehaviour
 {
+    [Header("Import parametrs")]
+    [SerializeField] private bool _import = false;
+    [SerializeField] private KartConfig _kartConfig;
+    [Header("RPM settings")]
+    [SerializeField] private float _idleRpm = 1000f;
+    [SerializeField] private float _maxRpm = 8000f;
+    [SerializeField] private float _revLimiterRpm = 7500f;
 
-    [SerializeField] float maxTorque = 400f;
-    [SerializeField] float idleRpm = 1000f;
-    [SerializeField] float maxRpm = 8000f;
+    [Header("Torque curve")]
+    [SerializeField] private AnimationCurve _torqueCurve;
 
-    [SerializeField] AnimationCurve torqueCurve;
+    [Header("Inertia & response")]
+    [SerializeField] private float _flywheelInertia = 0.2f;
+    [SerializeField] private float _throttleResponse = 5f;
 
+    [Header("Losses & load")]
+    [SerializeField] private float _engineFrictionCoeff = 0.02f;
+    [SerializeField] private float _loadTorqueCoeff = 5f;
 
+    public float CurrentRpm { get; private set; }
     public float CurrentTorque { get; private set; }
-    public float CurrentRPM { get; private set; }
+    public float SmoothedThrottle { get; private set; }
+    public float RevLimiterFactor { get; private set; } = 1f;
 
-    public float Simulate(float throttleInput, float forwardSpeed, float gearRatio, float wheelRadius, float deltaTime)
+    private float _invInertiaFactor;
+
+    private void Awake()
     {
-        float throttle = Mathf.Clamp01(throttleInput);
+        if (_import)
+        {
+            Initialize();
+        }
+        CurrentRpm = _idleRpm;
+        _invInertiaFactor = 60f / (2f * Mathf.PI * Mathf.Max(_flywheelInertia, 0.0001f));
+    }
 
-        float wheelOmewga = forwardSpeed / Mathf.Max(wheelRadius, float.MinValue);
-        float wheelRpm = wheelOmewga * 60 / (2 * Mathf.PI);
-        float kinematicRpm = Mathf.Abs(wheelRpm*gearRatio);
+    private void Initialize()
+    {
+        if (_kartConfig != null)
+        {
+            _torqueCurve = _kartConfig.engineTorqueCurve;
+            _invInertiaFactor = _kartConfig.engineInertia;
+            _maxRpm = _kartConfig.maxRpm;
+        }
+    }
+    public float Simulate(float throttleInput, float forwardSpeed, float deltaTime)
+    {
+        float targetThrottle = Mathf.Clamp01(throttleInput);
+        SmoothedThrottle = Mathf.MoveTowards(SmoothedThrottle, targetThrottle, _throttleResponse * deltaTime);
 
-        CurrentRPM = Mathf.Lerp(CurrentRPM, Mathf.Max(kinematicRpm, idleRpm), deltaTime);
-        CurrentRPM = Mathf.Clamp(CurrentRPM, idleRpm, maxRpm);
+        UpdateRevLimiterFactor();
 
-        float maxTorque = torqueCurve.Evaluate(CurrentRPM);
-        CurrentTorque = maxTorque * throttle;
+        float maxTorqueAtRpm = _torqueCurve.Evaluate(CurrentRpm);
 
+        float effectiveThrottle = SmoothedThrottle * RevLimiterFactor;
+        float driveTorque = maxTorqueAtRpm * effectiveThrottle;
+
+        float frictionTorque = _engineFrictionCoeff * CurrentRpm;
+        float loadTorque = _loadTorqueCoeff * Mathf.Abs(forwardSpeed);
+
+        float netTorque = driveTorque - frictionTorque - loadTorque;
+
+        float rpmDot = netTorque * _invInertiaFactor;
+        CurrentRpm += rpmDot * deltaTime;
+
+        if (CurrentRpm < _idleRpm) CurrentRpm = _idleRpm;
+        if (CurrentRpm > _maxRpm)  CurrentRpm = _maxRpm;
+
+        CurrentTorque = driveTorque;
         return CurrentTorque;
     }
 
+    private void UpdateRevLimiterFactor()
+    {
+        if (CurrentRpm <= _revLimiterRpm)
+        {
+            RevLimiterFactor = 1f;
+            return;
+        }
 
+        if (CurrentRpm >= _maxRpm)
+        {
+            RevLimiterFactor = 0f;
+            return;
+        }
+
+        float t = (CurrentRpm - _revLimiterRpm) / (_maxRpm - _revLimiterRpm);
+        RevLimiterFactor = 1f - t;
+    }
 }
